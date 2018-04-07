76a16f5b8d99ef1c5110825141510e8b0c9cdc79
Jean Carlo <jeancarlo_flores93@hotmail.com>
2018-04-07 17:53:26 -0500

    validation and styles

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import Media from './src/playlist/components/media'
 
 
 const app = document.getElementById('app')
-const holaMundo = <h1>Hola mundo!!!</h1>
-ReactDOM.render(holaMundo, app)
+//const holaMundo = <h1>Hola mundo!!!</h1>
+render(<Media title="¿Qué es responsive design" author="Jean Carlo Flores" image="./img/responsive.jpg" type="video"/>, app)
 console.log("Hello world")
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,5 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './media.css'
+import PropTypes from 'prop-types'
 
-class Media extends React.Component{
-    
+class Media extends Component {
+    render() {
+        const styles = {
+            container: {
+                color: 'red',
+                cursor: 'pointer',
+                border: '1px solid red',
+                width: 260
+            }
+        }
+        //UI
+        return (
+            <div className="Media">
+                <div className="Media-cover">
+                    <img src={ this.props.image }
+                        alt=""
+                        width={260}
+                        height={160} 
+                        className="Media-image"    
+                    />
+                    <p>Hola Mundo desde media.js</p>
+                    <h3 className="Media-title">{ this.props.title }</h3>
+                    <p className="Media-author">{ this.props.author }</p>
+                </div>
+            </div>
+        )
+    }
 }
+
+Media.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    type: PropTypes.oneOf(['video', 'audio'])
+    //si es para array
+    /*
+    .array
+    .number
+    .func
+    .object
+    */
+}
+export default Media
