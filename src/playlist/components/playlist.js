893c6a397d8174e80ca28bb65b31e33ceb4a08af
Jean Carlo <jeancarlo_flores93@hotmail.com>
2018-04-07 19:56:51 -0500

    Creating entrypoints

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Media from './media'
 import './playlist.css'
+import Play from '../../icons/components/play'
 
 function PlayList(props) {
     const playlist = props.data.categories[0].playlist
     console.log(props.data)
     return (
         <div className="Playlist">
+        <Play
+        size={50}
+        color="red"
+        />
             {
                 playlist.map((item) => {
                     return <Media {...item} key={item.id} />
