35d85474ff57a7e7431280eb707a7f45bfeb2d7e
Jean Carlo <jeancarlo_flores93@hotmail.com>
2018-04-07 20:28:55 -0500

    structure

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 //import Media from './src/playlist/components/media'
-import PlayList from './src/playlist/components/playlist'
-import data from './src/api.json'
+import Home from '../pages/containers/home'
+import PlayList from '../playlist/components/playlist'
+//import PlayList from './src/playlist/components/playlist'
+//import data from './src/api.json'
 
 
 const app = document.getElementById('app')
 //const holaMundo = <h1>Hola mundo!!!</h1>
 //render(<Media title="¿Qué es responsive design" author="Jean Carlo Flores" image="./img/responsive.jpg" type="video"/>, app)
-render(<PlayList data={data}/>, app)
+//render(<PlayList data={data}/>, app)
+render(<Home/>, app)
 console.log("Hello world")
