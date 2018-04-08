1a2706a75b0c514e55bc530dc334794c60d4679e
Jean Carlo <jeancarlo_flores93@hotmail.com>
2018-04-08 13:54:27 -0500

    play and pause

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout'
+import Video from '../components/video'
+import Title from '../components/title'
+import PlayPause from '../components/play-pause'
 
 class VideoPlayer extends Component {
+    state = {
+        pause: true
+    }
+
+    tooglePlay = () => {
+        this.setState({
+            pause: !this.state.pause
+        })
+    }
+
     render() {
         return (
             <VideoPlayerLayout>
-                <video
-                    controls
-                    autoPlay
+                <Title
+                    title="Esto es un video chido"
+                />
+                <PlayPause
+                pause={this.state.pause}
+                    handleClick={this.tooglePlay}
+                />
+                <Video
                     src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+                    autoplay={true}
                 />
             </VideoPlayerLayout>
         )
