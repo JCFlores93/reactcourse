06cf0cf49517ac59e6ab3385ad04e935c4480d4b
Jean Carlo <jeancarlo_flores93@hotmail.com>
2018-04-07 19:07:05 -0500

    render in playlist

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Media from './src/playlist/components/media'
+//import Media from './src/playlist/components/media'
+import PlayList from './src/playlist/components/playlist'
+import data from './src/api.json'
 
 
 const app = document.getElementById('app')
 //const holaMundo = <h1>Hola mundo!!!</h1>
-render(<Media title="¿Qué es responsive design" author="Jean Carlo Flores" image="./img/responsive.jpg" type="video"/>, app)
+//render(<Media title="¿Qué es responsive design" author="Jean Carlo Flores" image="./img/responsive.jpg" type="video"/>, app)
+render(<PlayList data={data}/>, app)
 console.log("Hello world")
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -43,15 +43,15 @@ class Media extends Component {
         return (
             <div className="Media" onClick={this.handleClick}>
                 <div className="Media-cover">
-                    <img src={this.props.image}
+                    <img src={this.props.cover}
                         alt=""
-                        width={260}
+                        width={240}
                         height={160}
                         className="Media-image"
                     />
                     <p>Hola Mundo desde media.js</p>
                     <h3 className="Media-title">{this.props.title}</h3>
-                    <p className="Media-author">{this.state.author}</p>
+                    <p className="Media-author">{this.props.author}</p>
                 </div>
             </div>
         )
@@ -59,7 +59,7 @@ class Media extends Component {
 }
 
 Media.propTypes = {
-    image: PropTypes.string,
+    cover: PropTypes.string,
     title: PropTypes.string.isRequired,
     author: PropTypes.string,
     type: PropTypes.oneOf(['video', 'audio'])
