749be4084289b8f76b9b9414065067c7408e7486
Jean Carlo <jeancarlo_flores93@hotmail.com>
2018-04-08 11:39:41 -0500

    onchange event

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './search.css'
-const Search = () => (
-    <form className="Search">
+const Search = (props) => (
+    <form
+        className="Search"
+        onSubmit={props.handleSubmit}
+    >
         <input
+            ref={props.setRef}
             type="text"
             className="Search-input"
             placeholder="Busca tu video favorito"
+            name="search"
+            defaultValue="Luis Fonsi"
+            onChange={props.handleChange}
+            value={props.value}
         />
     </form>
 )
