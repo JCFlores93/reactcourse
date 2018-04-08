84c2673d6c5362a3d3024ae5a0cdb425b54540cc
Jean Carlo <jeancarlo_flores93@hotmail.com>
2018-04-08 10:26:59 -0500

    handling errors

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -4,11 +4,13 @@ import Categories from '../../categories/components/categories'
 import Related from '../components/related'
 import ModalContainer from '../../widgets/containers/modal'
 import Modal from '../../widgets/components/modal'
+import HandleError from '../../error/containers/error'
 
 class Home extends Component {
 
     state = {
-        modalVisible: true
+        modalVisible: true,
+        //handleError: false
     }
 
     handleOpenModal = () => {
@@ -23,26 +25,37 @@ class Home extends Component {
         })
     }
 
+    /*componentDidCatch(error, info) {
+        this.setState({
+            handleError: true
+        })
+    }*/
     render() {
+        /*if (this.state.handleError) {
+            return <p>There's an error</p>
+        }*/
         return (
-            <HomeLayout>
-                <Related />
-                <Categories 
-                categories={this.props.data.categories} 
-                handleOpenModal={this.handleOpenModal}
-                />
-                {
-                    this.state.modalVisible &&
-                    <ModalContainer>
-                        <Modal
-                            handleClick={this.handleCloseModal}
-                        >
-                            <h1>Esto es un portal</h1>
-                        </Modal>
-                    </ModalContainer>
-                }
+            <HandleError>
+                <HomeLayout>
+                    <Related />
+                    <Categories
+                        categories={this.props.data.categories}
+                        handleOpenModal={this.handleOpenModal}
+                    />
+                    {
+                        this.state.modalVisible &&
+                        <ModalContainer>
+                            <Modal
+                                handleClick={this.handleCloseModal}
+                            >
+                                <h1>Esto es un portal</h1>
+                            </Modal>
+                        </ModalContainer>
+                    }
 
-            </HomeLayout>
+                </HomeLayout>
+            </HandleError>
+
         )
     }
 }
